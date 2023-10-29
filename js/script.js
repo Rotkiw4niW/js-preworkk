b2563299f9d640a95efbbbf2aaa5ea04809eba1b
Wiktor.Gebka <gwiktor11114@gmail.com>
2023-10-29 18:10:22 +0100

    add basic scructure HTML

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ function add(a, b){
 var numerki = add (5,7);
 
 console.log(numerki);
+
+var contents = getElementById("hello");
+var style = contents.style.display;
+console.log(style);
+function hideOrShow(){
+if(contents.style.display =block){
+contents.style.display= none;
+}else{
+contents.style.display= block;
+}
+};
